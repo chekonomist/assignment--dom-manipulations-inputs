ddc86e4660a3238c3140de82b42fe6d9598d2881
chekonomist <don.sergio.guerrero@gmail.com>
2018-06-04 10:52:28 -0500

    complete

--- main.js
+++ main.js
@@ -60,8 +60,24 @@ totalSumEl.innerHTML = `$ ${totalCheckSum}`
 })
 
 
-document.querySelector("#select-to-show-more button")
-.addEventListener('click',function(){
-  // TASK #4
+document
+.querySelector(“#select-to-show-more button”)
+.addEventListener(‘click’,function addCarInfo(){
+ // TASK #4
+ var carListDropDownEl = document.querySelector(‘.selection’)
+ var index = carListDropDownEl.selectedIndex
+ var imgEl = document.querySelector(‘.img-box’)
+ var makeAndModelEl = document.querySelector(‘.model’)
+ var msrpEl = document.querySelector(‘.msrp’)
+ var mpgEl = document.querySelector(‘.mpg’)
+ var edmundsEl = document.querySelector(‘.edmunds’)
 
+// Using this variable to read the data of EACH index
+ var datasetIndex = carListDropDownEl.options[index].dataset
+
+ imgEl.innerHTML = ‘<img src=´datasetIndex.img´>’
+ makeAndModelEl.innerHTML = carListDropDownEl.value
+ msrpEl.innerHTML = datasetIndex.msrp
+ mpgEl.innerHTML = datasetIndex.mpg
+ edmundsEl.innerHTML = datasetIndex.edmunds
 })
